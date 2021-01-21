9e777e124da460d4bd594383503561696ee3c25b
ShimizuNaoki <sensuikan1973@gmail.com>
2021-01-21 23:33:05 +0900

    add comment for Native Type Definition

--- lib/src/bindings/signatures.dart
+++ lib/src/bindings/signatures.dart
@@ -1,5 +1,8 @@
 import 'dart:ffi';
 import 'package:ffi/ffi.dart';
 
+// Native type is based on DLL_API of https://github.com/lavox/edax-reversi/tree/libedax.
+// If you know the list, clone the repository and switch the branch, and grep "DLL_API".
+
 typedef libedax_initialize_native_t = Int32 Function(Int32 argc, Pointer<Pointer<Utf8>> argv);
 typedef libedax_terminate_native_t = Int32 Function();
