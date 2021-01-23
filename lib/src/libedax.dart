efaa586e4b9ca069a00defc6bc00efccd5181b96
ShimizuNaoki <sensuikan1973@gmail.com>
2021-01-24 03:37:31 +0900

    how is this?

--- lib/src/libedax.dart
+++ lib/src/libedax.dart
@@ -33,7 +33,7 @@ class LibEdax {
   /// If you want to know more, See [Options Document](https://sensuikan1973.github.io/edax-reversi/structOptions.html).
   void libedaxInitialize([List<String> args = const []]) {
     final argsPointers = args.map(Utf8.toUtf8).toList();
-    final pointerPointer = allocate<Pointer<Utf8>>(count: argsPointers.length + 1);
+    final pointerPointer = allocate<Pointer<Utf8>>(count: argsPointers.length);
     for (var k = 0; k < argsPointers.length; k++) {
       pointerPointer[k] = argsPointers[k];
     }
