18337752a43991c8e5aa6e071ae02ed774156e9a
ShimizuNaoki <sensuikan1973@gmail.com>
2021-01-24 03:40:44 +0900

    how is this?

--- test/libedax4dart_test.dart
+++ test/libedax4dart_test.dart
@@ -12,42 +12,46 @@ void main() {
       ..libedaxTerminate();
   });
 
-  // test('initialize without args, and set option', () {
-  //   const LibEdax()
-  //     ..libedaxInitialize()
-  //     ..edaxVersion()
-  //     ..edaxSetOption('level', '15')
-  //     ..libedaxTerminate();
-  // });
+  test('initialize without args, and set option', () {
+    const LibEdax()
+      ..libedaxInitialize()
+      ..edaxInit()
+      ..edaxVersion()
+      ..edaxSetOption('level', '15')
+      ..libedaxTerminate();
+  });
 
-  // test('play "horse" opening', () {
-  //   final edax = const LibEdax()
-  //     ..libedaxInitialize()
-  //     ..edaxBookOn()
-  //     ..edaxPlay('f5d6c5f4d3');
-  //   expect(edax.edaxOpening(), 'horse');
-  //   edax.libedaxTerminate();
-  // });
+  test('play "horse" opening', () {
+    final edax = const LibEdax()
+      ..libedaxInitialize()
+      ..edaxInit()
+      ..edaxBookOn()
+      ..edaxPlay('f5d6c5f4d3');
+    expect(edax.edaxOpening(), 'horse');
+    edax.libedaxTerminate();
+  });
 
-  // test('setBoard', () {
-  //   const boardString = '-W----W--------------------WB------WBB-----W--------------------B';
-  //   final edax = const LibEdax()
-  //     ..libedaxInitialize()
-  //     ..edaxSetboard(boardString);
-  //   expect(edax.edaxGetDisc(edax.white), 'W'.allMatches(boardString).length);
-  //   expect(edax.edaxGetDisc(edax.black), 'B'.allMatches(boardString).length - 1);
-  //   expect(edax.edaxGetCurrentPlayer(), edax.black);
-  //   edax.libedaxTerminate();
-  // });
+  test('setBoard', () {
+    const boardString = '-W----W--------------------WB------WBB-----W--------------------B';
+    final edax = const LibEdax()
+      ..libedaxInitialize()
+      ..edaxInit()
+      ..edaxSetboard(boardString);
+    expect(edax.edaxGetDisc(edax.white), 'W'.allMatches(boardString).length);
+    expect(edax.edaxGetDisc(edax.black), 'B'.allMatches(boardString).length - 1);
+    expect(edax.edaxGetCurrentPlayer(), edax.black);
+    edax.libedaxTerminate();
+  });
 
-  // test('check mobility count', () {
-  //   final edax = const LibEdax()
-  //     ..libedaxInitialize()
-  //     ..edaxBookRandomness(2)
-  //     ..edaxGo();
-  //   expect(edax.edaxGetMobilityCount(edax.white), 3);
-  //   edax.libedaxTerminate();
-  // });
+  test('check mobility count', () {
+    final edax = const LibEdax()
+      ..libedaxInitialize()
+      ..edaxInit()
+      ..edaxBookRandomness(2)
+      ..edaxGo();
+    expect(edax.edaxGetMobilityCount(edax.white), 3);
+    edax.libedaxTerminate();
+  });
 
   // test('play a short game until game over', () {
   //   const initParams = ['', '-eval-file', 'data/eval.dat', '-book-file', 'data/book.dat', '-level', '16'];
