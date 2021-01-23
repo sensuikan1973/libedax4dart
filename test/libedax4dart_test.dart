5c49cb80bf529e0996d7029eba72e1d61f63e236
ShimizuNaoki <sensuikan1973@gmail.com>
2021-01-24 02:53:30 +0900

    how is this?

--- test/libedax4dart_test.dart
+++ test/libedax4dart_test.dart
@@ -39,27 +39,27 @@ void main() {
     edax.libedaxTerminate();
   });
 
-  // test('play a short game until game over', () {
-  //   const initParams = ['']; // , '-eval-file', 'data/eval.dat', '-book-file', 'data/book.dat', '-level', '16'];
-  //   final edax = const LibEdax()
-  //     ..libedaxInitialize(initParams)
-  //     ..edaxInit()
-  //     ..edaxMode(3) // human vs human
-  //     ..edaxMove('f5');
-  //   expect(edax.edaxCanMove(), true);
-  //   expect(edax.edaxGetCurrentPlayer(), edax.white);
-  //   expect(edax.edaxIsGameOver(), false);
-  //   edax.edaxPlay('d6C5F4e3f6g5e6e7'); // famous perfect game. BLACK win.
-  //   expect(edax.edaxIsGameOver(), true);
-  //   expect(edax.edaxGetDisc(edax.white), 0);
-  //   expect(edax.edaxGetDisc(edax.black), 13);
-  //   // expect(_radix16board(edax.edaxGetBoard().player), List<String>.filled(16, '0').join()); // white bitboard
-  //   // expect(_radix16board(edax.edaxGetBoard().opponent), '0010387c38100000'); // black bitboard
-  //   // expect(edax.edaxCanMove(), false);
-  //   // expect(edax.edaxGetLastMove().x, 52); // e7 is 52th. (a1 is 0th)
-  //   // expect(edax.edaxGetMoves(), 'F5d6C5f4E3f6G5e6E7'); // edax return moves with upper scale B move and lower scale W.
-  //   edax.libedaxTerminate();
-  // });
+  test('play a short game until game over', () {
+    const initParams = ['', '-eval-file', 'data/eval.dat', '-book-file', 'data/book.dat', '-level', '16'];
+    final edax = const LibEdax()
+      ..libedaxInitialize()
+      ..edaxInit()
+      ..edaxMode(3) // human vs human
+      ..edaxMove('f5');
+    expect(edax.edaxCanMove(), true);
+    expect(edax.edaxGetCurrentPlayer(), edax.white);
+    expect(edax.edaxIsGameOver(), false);
+    edax.edaxPlay('d6C5F4e3f6g5e6e7'); // famous perfect game. BLACK win.
+    expect(edax.edaxIsGameOver(), true);
+    expect(edax.edaxGetDisc(edax.white), 0);
+    expect(edax.edaxGetDisc(edax.black), 13);
+    // expect(_radix16board(edax.edaxGetBoard().player), List<String>.filled(16, '0').join()); // white bitboard
+    // expect(_radix16board(edax.edaxGetBoard().opponent), '0010387c38100000'); // black bitboard
+    // expect(edax.edaxCanMove(), false);
+    // expect(edax.edaxGetLastMove().x, 52); // e7 is 52th. (a1 is 0th)
+    // expect(edax.edaxGetMoves(), 'F5d6C5f4E3f6G5e6E7'); // edax return moves with upper scale B move and lower scale W.
+    edax.libedaxTerminate();
+  });
 
   // test('get book move', () {
   //   final edax = const LibEdax()
