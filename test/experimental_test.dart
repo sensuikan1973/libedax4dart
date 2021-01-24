d887a471235cc6ee7fa93ba19ce0b565ff42f952
ShimizuNaoki <sensuikan1973@gmail.com>
2021-01-25 05:47:51 +0900

    experimental test

---
+++ test/experimental_test.dart
@@ -0,0 +1,49 @@
+import 'package:libedax4dart/libedax4dart.dart';
+import 'package:test/test.dart';
+
+const testBookFile = './resources/test_book.dat';
+
+void main() {
+  // pub run test . --tags exp
+  group('experimental test for DEBUG', () {
+    test('initialize without args', () {
+      LibEdax()
+        ..libedaxInitialize()
+        ..edaxInit()
+        ..edaxVersion()
+        ..libedaxTerminate();
+    });
+
+    test('initialize without args', () {
+      LibEdax()
+        ..libedaxInitialize()
+        ..edaxInit()
+        ..edaxVersion()
+        ..libedaxTerminate();
+    });
+
+    test('initialize without args', () {
+      LibEdax()
+        ..libedaxInitialize()
+        ..edaxInit()
+        ..edaxVersion()
+        ..libedaxTerminate();
+    });
+
+    test('initialize without args', () {
+      LibEdax()
+        ..libedaxInitialize()
+        ..edaxInit()
+        ..edaxVersion()
+        ..libedaxTerminate();
+    });
+
+    test('initialize without args', () {
+      LibEdax()
+        ..libedaxInitialize()
+        ..edaxInit()
+        ..edaxVersion()
+        ..libedaxTerminate();
+    });
+  }, tags: 'exp');
+}
