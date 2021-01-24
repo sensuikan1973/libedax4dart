673e5755a2a6f2decea486e5a63b551dd9d714e6
done <23427957+sensuikan1973@users.noreply.github.com>
2021-01-24 22:46:53 +0900

    test coverage (#19)
    * add coverage package. See: https://github.com/dart-lang/test/issues/1265
    * use codecov
    * add codecov badge
    * fix test/coverage command & add .codecov.yml
    * remove unused factory
    * codecov ignore ffi structs

--- lib/src/bindings/structs/move.dart
+++ lib/src/bindings/structs/move.dart
@@ -1,21 +1,6 @@
 import 'dart:ffi';
-import 'package:ffi/ffi.dart';
 
 class Move extends Struct {
-  factory Move.allocate(
-    int flipped,
-    int x,
-    int score,
-    int cost,
-    Pointer<Move> next,
-  ) =>
-      allocate<Move>().ref
-        ..flipped = flipped
-        ..x = x
-        ..score = score
-        ..cost = cost
-        ..next = next;
-
   /// bitboard representation of flipped squares.
   @Uint64()
   external int flipped;
