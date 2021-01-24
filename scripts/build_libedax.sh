8b0264f7b6e6023a2566602faaf9cd966cd988b4
ShimizuNaoki <sensuikan1973@gmail.com>
2021-01-25 07:19:51 +0900

    change dylib compile flag

--- scripts/build_libedax.sh
+++ scripts/build_libedax.sh
@@ -5,8 +5,8 @@
 # TODO: https://github.com/lavox/edax-reversi/pull/2 がマージされたら参照先を変える
 git clone https://github.com/sensuikan1973/edax-reversi
 cd edax-reversi
-git switch fix_command_shared_lib_for_linux
-git checkout 50ff6d9bffdf9661707a98decbd7152f254a848a
+git switch fix_broken_so_or_dll
+# git checkout 50ff6d9bffdf9661707a98decbd7152f254a848a
 
 mkdir -p data
 curl -OL https://github.com/abulmo/edax-reversi/releases/download/v4.4/eval.7z
