d2e60304d2534c411f169ac72411c1d7057175e8
ShimizuNaoki <sensuikan1973@gmail.com>
2021-01-24 22:36:32 +0900

    remove unused factory

--- lib/src/bindings/structs/board.dart
+++ lib/src/bindings/structs/board.dart
@@ -1,11 +1,6 @@
 import 'dart:ffi';
-import 'package:ffi/ffi.dart';
 
 class Board extends Struct {
-  factory Board.allocate(int player, int opponent) => allocate<Board>().ref
-    ..player = player
-    ..opponent = opponent;
-
   @Uint64()
   external int player;
 
