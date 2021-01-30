872b6a66d0169cbdb19c89513447f7902751c209
ShimizuNaoki <sensuikan1973@gmail.com>
2021-01-31 05:01:43 +0900

    use my repo

--- scripts/build_libedax.sh
+++ scripts/build_libedax.sh
@@ -2,10 +2,10 @@
 # libedax_build_command (e.g. make libbuild ARCH=x64-modern COMP=gcc OS=osx)
 # dst: (e.g. build)
 
-git clone https://github.com/lavox/edax-reversi
+git clone https://github.com/sensuikan1973/edax-reversi
 cd edax-reversi
-git switch libedax
-git checkout 985ef418833a4bf7a9a59f4d6104c91c39b7c83e
+git switch libedax_sensuikan1973
+git checkout f24103d228aecbaa66bf4cb2477f8864c0b14356
 
 mkdir -p data
 curl -OL https://github.com/abulmo/edax-reversi/releases/download/v4.4/eval.7z
