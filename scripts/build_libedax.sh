1a34f26d2d9b3f246d22006a687da827bb8f3ff7
ShimizuNaoki <sensuikan1973@gmail.com>
2021-01-31 04:50:15 +0900

    use lavox repo branch. See: https://github.com/lavox/edax-reversi/pull/2

--- scripts/build_libedax.sh
+++ scripts/build_libedax.sh
@@ -2,11 +2,10 @@
 # libedax_build_command (e.g. make libbuild ARCH=x64-modern COMP=gcc OS=osx)
 # dst: (e.g. build)
 
-# See: https://github.com/lavox/edax-reversi/pull/2
-# See: https://github.com/sensuikan1973/edax-reversi/pull/2
-git clone https://github.com/sensuikan1973/edax-reversi
+git clone https://github.com/lavox/edax-reversi
 cd edax-reversi
-git switch libedax_sensuikan1973
+git switch libedax
+git checkout 985ef418833a4bf7a9a59f4d6104c91c39b7c83e
 
 mkdir -p data
 curl -OL https://github.com/abulmo/edax-reversi/releases/download/v4.4/eval.7z
