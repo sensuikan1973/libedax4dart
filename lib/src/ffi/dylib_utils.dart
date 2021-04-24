e90264f34640888767f4ecb27e35ab9d6e76642e
ShimizuNaoki <sensuikan1973@gmail.com>
2021-04-25 06:07:17 +0900

    fix BROKEN integration test on windows integration

--- lib/src/ffi/dylib_utils.dart
+++ lib/src/ffi/dylib_utils.dart
@@ -1,6 +1,8 @@
 import 'dart:ffi';
 import 'dart:io' show Directory, Platform;
 
+import 'package:path/path.dart' as p;
+
 String get _libName {
   if (Platform.isLinux || Platform.isAndroid) return 'libedax.so';
   if (Platform.isMacOS) return 'libedax.dylib';
@@ -9,4 +11,4 @@ String get _libName {
 }
 
 DynamicLibrary dlopenPlatformSpecific([String dllPath = '']) =>
-    dllPath.isEmpty ? DynamicLibrary.open('${Directory.current.path}/$_libName') : DynamicLibrary.open(dllPath);
+    dllPath.isEmpty ? DynamicLibrary.open(p.join(Directory.current.path, _libName)) : DynamicLibrary.open(dllPath);
