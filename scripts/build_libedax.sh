d379f29a0fc44fd051dfa3e4e3f32c582942f9fc
done <23427957+sensuikan1973@users.noreply.github.com>
2021-04-26 06:51:58 +0900

    update CI (#54)
    * mild fix comment
    
    * enhance test
    
    * output dart version
    
    * remove unnecessary rm -rf
    
    * also run output_build_info script in integration test
    
    * fix managing libedax version
    
    * specify script

--- scripts/build_libedax.sh
+++ scripts/build_libedax.sh
@@ -1,11 +1,14 @@
 # NOTE: require some environment variables.
 # libedax_build_command (e.g. make libbuild ARCH=x64-modern COMP=gcc OS=osx)
 # dst: (e.g. build)
+#
+# example:
+# libedax_build_command="make libbuild ARCH=x64-modern COMP=gcc OS=osx" dst="." ./scripts/build_libedax.sh
 
 git clone https://github.com/sensuikan1973/edax-reversi
 cd edax-reversi
 git switch libedax_sensuikan1973
-git checkout f24103d228aecbaa66bf4cb2477f8864c0b14356
+git checkout $(cat ../libedax-version)
 
 mkdir -p data
 curl -OL https://github.com/abulmo/edax-reversi/releases/download/v4.4/eval.7z
@@ -16,7 +19,6 @@ cd src
 $libedax_build_command
 
 cd ../../
-rm -rf $dst
 mkdir -p $dst
 
 cp -r edax-reversi/bin $dst/bin
--- scripts/output_build_info.sh
+++ scripts/output_build_info.sh
@@ -9,15 +9,13 @@ echo "=== libedax4dart sha ===" >> $dst_file
 echo $GITHUB_SHA >> $dst_file
 
 echo "=== libedax sha ===" >> $dst_file
-cd edax-reversi
-git rev-parse HEAD >> ../$dst_file
-cd ..
+cat libedax-version >> $dst_file
 
 echo "=== os image ===" >> $dst_file
 echo $ImageOS >> $dst_file
 
-# echo "=== dart version ===" >> $dst_file
-# dart --version >> $dst_file 2>&1
+echo "=== dart version ===" >> $dst_file
+dart --version >> $dst_file 2>&1
 
 echo "=== $2 version ===" >> $dst_file
 $2 --version >> $dst_file
