0f7df9e7471fb28230691578b5e36eaa0cfca451
ShimizuNaoki <sensuikan1973@gmail.com>
2021-04-28 02:28:27 +0900

    mild fix comment

--- lib/src/best_path_num_to_win_with_link.dart
+++ lib/src/best_path_num_to_win_with_link.dart
@@ -6,12 +6,12 @@ import 'link.dart';
 class BestPathNumToWinWithLink {
   const BestPathNumToWinWithLink(this.bestPathNumOfCurrentPlayer, this.bestPathNumOfOpponentPlayer, this.link);
 
-  /// best path list to win for current player.
+  /// best path num to win for current player.
   ///
   /// this means that if you learn these pathes, you win to opposite best moves definitely.
   final int bestPathNumOfCurrentPlayer;
 
-  /// best path list to win for opponent player.
+  /// best path num to win for opponent player.
   ///
   /// this means that if you learn these pathes, you win to opposite best moves definitely.
   final int bestPathNumOfOpponentPlayer;
