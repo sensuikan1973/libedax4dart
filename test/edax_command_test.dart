fc17faa868dee174dee874fbabc423b10fe794b8
ShimizuNaoki <sensuikan1973@gmail.com>
2021-05-04 08:07:25 +0900

    refactor test of LibEdax util method

--- test/edax_command_test.dart
+++ test/edax_command_test.dart
@@ -302,5 +302,15 @@ void main() {
         edax.libedaxTerminate();
       });
     });
+
+    group('util command', () {
+      test('popCount', () {
+        final edax = LibEdax();
+        expect(edax.popCount(7), 3); // 0000 0111
+        expect(edax.popCount(14), 3); // 0000 1110
+        expect(edax.popCount(15), 4); // 0000 1111
+        expect(edax.popCount(17), 2); // 0001 0001
+      });
+    });
   });
 }
