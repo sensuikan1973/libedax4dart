bc89266f2d33a32841bb100ece4c838cf0a6640d
ShimizuNaoki <sensuikan1973@gmail.com>
2021-05-09 02:43:28 +0900

    update libedax version

--- scripts/build_libedax.sh
+++ scripts/build_libedax.sh
@@ -9,6 +9,8 @@ git clone https://github.com/sensuikan1973/edax-reversi
 cd edax-reversi
 git remote update --prune
 
+git checkout .
+
 git switch libedax_sensuikan1973
 git pull
 git checkout $(cat ../.libedax-version)
