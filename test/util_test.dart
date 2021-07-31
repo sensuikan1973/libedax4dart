12da9b1cb36b0f9190a7a858edfad52044f66015
ShimizuNaoki <sensuikan1973@gmail.com>
2021-08-01 08:30:56 +0900

    mild enhance util test

--- test/util_test.dart
+++ test/util_test.dart
@@ -2,6 +2,18 @@ import 'package:libedax4dart/libedax4dart.dart';
 import 'package:test/test.dart';
 
 void main() {
+  test('move2String', () {
+    expect(move2String(0), 'a1');
+    expect(move2String(25), 'b4');
+    expect(move2String(63), 'h8');
+  });
+
+  test('score2String', () {
+    expect(score2String(0), '0');
+    expect(score2String(10), '+10');
+    expect(score2String(-7), '-7');
+  });
+
   group('symetryMove', () {
     test('0: same place', () {
       const moveC2 = 10;
