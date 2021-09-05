bdf492ecb2e8cc7e50c4a4c4a940bd12c5335ffc
done <23427957+sensuikan1973@users.noreply.github.com>
2021-09-06 02:48:02 +0900

    delete deprecated functions (#83)
    * delete deprecated functions
    
    * bump version
    
    * analyzer
    
    * fix file name

--- lib/libedax4dart.dart
+++ lib/libedax4dart.dart
@@ -1,10 +1,9 @@
 /// dart wrapper for libedax
 library libedax4dart;
 
-export 'src/best_path_num_with_link.dart';
 export 'src/board.dart';
 export 'src/constants.dart';
-export 'src/count_best_path_result.dart';
+export 'src/count_bestpath_result.dart';
 export 'src/hint.dart';
 export 'src/libedax.dart';
 export 'src/link.dart';
