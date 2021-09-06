8ea3a122d366bc1d32e56178acc67bdc073e1cf2
done <23427957+sensuikan1973@users.noreply.github.com>
2021-09-06 11:52:23 +0900

    Update pointer_int8.dart

--- lib/extensions/pointer_int8.dart
+++ lib/extensions/pointer_int8.dart
@@ -5,5 +5,6 @@ import 'dart:ffi';
 import 'package:ffi/ffi.dart';
 
 extension Int8PointerString on Pointer<Int8> {
+  // See: https://pub.dev/packages/ffigen#how-does-ffigen-handle-c-strings
   String toStr() => cast<Utf8>().toDartString();
 }
