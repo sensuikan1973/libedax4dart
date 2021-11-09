72b2bf9a5439ca6b6de54df221ad2cdd9378847b
sensuikan1973 <sensuikan1973@gmail.com>
2021-11-09 14:12:26 +0900

    enhance test

--- test/edax_command_test.dart
+++ test/edax_command_test.dart
@@ -67,15 +67,17 @@ void main() {
         ..edaxInit()
         ..edaxPlay('f5d6c5f4d3');
       final board = edax.edaxGetBoard();
+      final stringApplicableToSetboard = board.stringApplicableToSetboard(edax.edaxGetCurrentPlayer());
+      expect(
+        stringApplicableToSetboard,
+        '-------------------*-------**O----**O*-----O--------------------W',
+      );
+      edax.edaxSetboard(stringApplicableToSetboard);
       expect(edax.edaxGetCurrentPlayer(), TurnColor.white);
       expect(board.squaresOfPlayer, [29, 36, 43]);
       expect(board.squareStringsOfPlayer, ['f4', 'e5', 'd6']);
       expect(board.squaresOfOpponent, [19, 27, 28, 34, 35, 37]);
       expect(board.squareStringsOfOpponent, ['d3', 'd4', 'e4', 'c5', 'd5', 'f5']);
-      expect(
-        board.stringApplicableToSetboard(edax.edaxGetCurrentPlayer()),
-        '-------------------*-------**O----**O*-----O--------------------W',
-      );
       stdout.writeln(board.prettyString(edax.edaxGetCurrentPlayer()));
       edax.libedaxTerminate();
     });
