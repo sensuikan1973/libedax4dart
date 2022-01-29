82355e291c590f7fad6fe102beb58751ef07cad4
sensuikan1973 <sensuikan1973@gmail.com>
2022-01-29 20:06:10 +0900

    mild fix

--- scripts/build_libedax.sh
+++ scripts/build_libedax.sh
@@ -12,9 +12,6 @@ rm -rf edax-reversi
 git clone https://github.com/sensuikan1973/edax-reversi
 cd edax-reversi
 git remote update --prune
-
-git checkout .
-
 git switch libedax_sensuikan1973
 git checkout $(cat ../.libedax-version)
 
