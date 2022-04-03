1efd2ed9d2c7dbdbbce5505c05126694c9138577
sensuikan1973 <sensuikan1973@gmail.com>
2022-04-03 23:07:26 +0900

    fix SC2046

--- scripts/build_libedax.sh
+++ scripts/build_libedax.sh
@@ -14,7 +14,7 @@ git clone https://github.com/sensuikan1973/edax-reversi
 cd edax-reversi
 git remote update --prune
 git switch libedax_sensuikan1973
-git checkout $(cat ../.libedax-version)
+git checkout "$(cat ../.libedax-version)"
 
 mkdir -p data
 curl -OL https://github.com/abulmo/edax-reversi/releases/download/v4.4/eval.7z
