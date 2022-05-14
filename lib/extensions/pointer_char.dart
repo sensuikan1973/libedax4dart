e04a7e8dbedd1931cc8a5722799d62fd808490e1
sensuikan1973 <sensuikan1973@gmail.com>
2022-05-15 01:51:58 +0900

    follow dart ffi change, Char

--- lib/extensions/pointer_char.dart
+++ lib/extensions/pointer_char.dart
@@ -4,7 +4,7 @@ import 'dart:ffi';
 
 import 'package:ffi/ffi.dart';
 
-extension Int8PointerString on Pointer<Int8> {
+extension CharPointerString on Pointer<Char> {
   // See: https://pub.dev/packages/ffigen#how-does-ffigen-handle-c-strings
-  String toStr() => cast<Utf8>().toDartString();
+  String toDartStr() => cast<Utf8>().toDartString();
 }
