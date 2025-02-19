b070ef572a3d487a812cd0ca854267a760b063c5
sensuikan1973 <sensuikan1973@gmail.com>
2025-02-19 14:48:58 +0900

    d

--- test/edax_command_test.dart
+++ test/edax_command_test.dart
@@ -401,6 +401,7 @@ void main() {
       const opening = 'f5f4f3';
       edax
         ..edaxInit()
+        ..edaxBookSave(bookFile)
         ..edaxPlay(opening);
       final position = edax.edaxGetBookMoveWithPosition().position;
       expect(position.nLink, 0);
@@ -408,7 +409,6 @@ void main() {
         ..edaxMode(2) // edax vs edax
         ..edaxGo()
         ..edaxPlayPrint()
-        ..edaxBookSave(bookFile)
         ..edaxBookStore()
         ..edaxBookSave(bookFile)
         ..edaxMode(3) // human vs human
