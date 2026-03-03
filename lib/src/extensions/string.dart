1af428da0614449d5957a0a37e7826e8cd86e4ee
sensuikan1973 <sensuikan1973@gmail.com>
2026-03-03 23:27:19 +0900

    remove Windows-specific handling in toCharPointer method

--- lib/src/extensions/string.dart
+++ lib/src/extensions/string.dart
@@ -1,21 +1,10 @@
 // See: https://github.com/dart-lang/ffigen/issues/72
 
 import 'dart:ffi';
-import 'dart:io';
 
 import 'package:ffi/ffi.dart';
 
 extension CharPointer on String {
-  Pointer<Char> toCharPointer(Allocator allocator) {
-    if (Platform.isWindows) {
-      final bytes = systemEncoding.encode(this);
-      final pointer = allocator<Uint8>(bytes.length + 1);
-      final buffer = pointer.asTypedList(bytes.length + 1);
-      buffer.setAll(0, bytes);
-      buffer[bytes.length] = 0;
-      return pointer.cast<Char>();
-    }
-
-    return toNativeUtf8(allocator: allocator).cast<Char>();
-  }
+  Pointer<Char> toCharPointer(Allocator allocator) =>
+      toNativeUtf8(allocator: allocator).cast<Char>();
 }
